9b10eb8c35e136d1dc766458079cc3efda0956e3
williamruize <72904098+williamruize@users.noreply.github.com>
2020-11-22 21:04:29 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -7,12 +7,25 @@
 </head>
 
 <body>
+	
+	
+	    
+	    <div>"userId"<input id="userId"  type="number"></div>
+	    <div>"Id"<input  id="id" type="number"></div>
+		<div>"title"<input id="title"  type="text"></div>
+	    <div>"completed"<input id="completed"  type="text"></div>
+	 
+	<button type="button" id="btn_guardar" onclick="guardar()">Guardar</button>
+	<button type="button" id="btn_actualizar" onclick="Actualizar()"  style="display:none">Actualizar</button>
+	<button type="button" onclick="limpiar()">Limpiar</button>
+	
+	
 	<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 	<script src="js/controlador.js"></script>
 
 	
 <table id="tablainformacion" width="200" border="1">
-  <tbody1>
+  <thead>
     <tr>
       <th scope="col">UserId</th>
       <th scope="col">ID </th>
@@ -20,8 +33,8 @@
       <th scope="col">Completed</th>
 	<th scope="col">Acciones</th>
     </tr>
-	  </tbody1>
-	<tbody>
+	</thead>
+    <tbody>
   </tbody>
 </table>
 
--- js/controlador.js
+++ js/controlador.js
@@ -1,5 +1,6 @@
 // JavaScript Document
-var informacion = []
+var informacion = [];
+var actualiza ;
 const url='https://jsonplaceholder.typicode.com/todos/1';
 function obtenerinformacion(){
 axios({
@@ -17,14 +18,15 @@ axios({
 obtenerinformacion();
 
 function llenarinformacion(){
-	//document.querySelector('#tablainformacion tbody').innerHTML=``;
+	document.querySelector('#tablainformacion tbody').innerHTML=``;
 	
 	document.querySelector('#tablainformacion tbody').innerHTML+=`<tr>
 	<th scope="col">${informacion.userId}</th>
       <th scope="col">${informacion.id}</th>
       <th scope="col">${informacion.title}</th>
 	<th scope="col">${informacion.completed}</th>
-	<th scope="col"><button type="button" onclick="eliminar(${informacion.id})">X</button></th>
+	<th scope="col"><button type="button" onclick="eliminar(${informacion.id})">X</button>
+<button type="button" onclick="editar(${informacion.id})">Editar</button></th>
 	</tr>`;
 	
 }
@@ -34,11 +36,84 @@ function eliminar(indice){
 	axios({
 	method: 'DELETE',
 	url:url +'?id='+indice, 
+	responseType:'json',
+	
+}).then(res=>{
+	console.log(res.data);
+	obtenerinformacion();	
+}).catch(error=>{
+	console.error(error)});
+}
+function guardar(){
+	let informacion={
+		userId: document.getElementById('userId').value,
+		id: document.getElementById('id').value,
+		title: document.getElementById('title').value,
+		completed: document.getElementById('completed').value,
+	}
+	console.log ('informacion a guardar ', informacion)
+axios({
+	method: 'POST',
+	url:url, 
+	responseType:'json',
+	data:informacion
+}).then(res=>{
+	console.log(res);
+	limpiar();
+	llenarinformacion();
+}).catch(error=>{
+	console.error(error);
+});
+}
+function limpiar(){
+	
+	document.getElementById('userId').value=null;
+	document.getElementById('id').value=null;
+	document.getElementById('title').value=null;
+	document.getElementById('completed').value=null;
+	document.getElementById('btn_guardar').style.display='inline';
+	document.getElementById('btn_actualizar').style.display='none';
+	}
+function Actualizar(){
+	
+		let informacion={
+		userId: document.getElementById('userId').value,
+		id: document.getElementById('id').value,
+		title: document.getElementById('title').value,
+		completed: document.getElementById('completed').value,
+	}
+	console.log ('informacion a actualizar ', informacion)
+axios({
+	method: 'PUT',
+	url:url, 
+	responseType:'json',
+	data:informacion
+}).then(res=>{
+	console.log(res);
+	limpiar();
+	llenarinformacion();
+}).catch(error=>{
+	console.error(error);
+});
+	}	
+function editar(indice){
+	actualiza= indice ;
+		console.log('Se selecciono '+indice)
+	axios({
+	method: 'GET',
+	url:url, 
 	responseType:'json'
 	
 }).then(res=>{
 	console.log(res.data);
-		
+	document.getElementById('userId').value=res.data.userId;
+	document.getElementById('id').value=res.data.id;
+	document.getElementById('title').value=res.data.title;
+	document.getElementById('completed').value=res.data.completed;
+	document.getElementById('btn_guardar').style.display='none';
+	document.getElementById('btn_actualizar').style.display='inline';	
 }).catch(error=>{
 	console.error(error)});
 }
+			
+		
